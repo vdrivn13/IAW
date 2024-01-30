5a154b2ab4ac82a656f4ea47a36a0989ea8ab1ae
vdrivn13 <157021332+vdrivn13@users.noreply.github.com>
2024-01-30 20:06:25 +0100

    Update ej4.php

--- Relacion4/ej4.php
+++ Relacion4/ej4.php
@@ -1,9 +1,13 @@
 <?php
-
+/*Crea una biblioteca de funciones para arrays (de una dimensión) de números enteros que contenga las siguientes funciones:
+a. generaArrayInt: Genera un array de tamaño n con números aleatorios cuyo
+intervalo es 0, 100.
+b. minimoArrayInt: Devuelve el mínimo del array que se pasa como parámetro.
+c. maximoArrayInt: Devuelve el máximo del array que se pasa como parámetro.*/
 class FuncionesArrays {
-    function generaArrayInt($tamanio) {
+    function generaArrayInt($tamaño) {
         $array = array();
-        for ($i = 0; $i < $tamanio; $i++) {
+        for ($i = 0; $i < $tamaño; $i++) {
             $array[] = rand(0, 100);
         }
         return $array;
