e12b2d18653edeee8ec68b1ddc3cac476d622dc3
vdrivn13 <157021332+vdrivn13@users.noreply.github.com>
2024-01-30 20:03:49 +0100

    Update ej2y3.php

--- Relacion4/ej2y3.php
+++ Relacion4/ej2y3.php
@@ -1,20 +1,20 @@
 <?php
+include 'ej1.php';
+// Usando la librería del ejercicio 1. Muestra los números primos que hay entre 1 y 1000.
+echo "Números primos entre 1 y 1000:\n";
+for ($i = 1; $i <= 1000; $i++) {
+    if (esPrimo($i)) {
+        echo $i . " ";
+    }
+}
 
-include 'funciones.php';
-   function esCapicua($num) {
-        $numInvertido = strrev($num);
-        return $num == $numInvertido;
+
+include 'ej1.php';
+// Usando la librería del ejercicio 1. Muestra los números capicúa que hay entre 1 y 99999.
+echo "\n\nNúmeros capicúa entre 1 y 99999:\n";
+for ($i = 1; $i <= 99999; $i++) {
+    if (esCapicua($i)) {
+        echo $i . " ";
     }
-    
-    function esPrimo($num) {
-        if ($num < 2) {
-            return false;
-        }
-        for ($i = 2; $i <= sqrt($num); $i++) {
-            if ($num % $i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+}
 ?>
