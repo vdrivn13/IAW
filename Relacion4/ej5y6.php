836c6f719ada64cf39226470b29a9cb228acbc52
vdrivn13 <157021332+vdrivn13@users.noreply.github.com>
2024-01-30 20:09:05 +0100

    Update ej5y6.php

--- Relacion4/ej5y6.php
+++ Relacion4/ej5y6.php
@@ -1,7 +1,14 @@
 <?php
-
+// Incluye las funciones de la biblioteca anterior
 include 'ej4.php';
 
+// Genera un array de tamaño 10 con números aleatorios
+$tamanoArray = 10;
+$arrayAleatorio = generaArrayInt($tamanoArray);
 
+// Muestra el array
+echo "Array: " . implode(", ",$arrayAleatorio) . "\n";
 
-?>
+// Muestra el número máximo y mínimo del array
+echo "Número máximo del array: " . maximoArrayInt($arrayAleatorio) . "\n";
+echo "Número mínimo del array: " . minimoArrayInt($arrayAleatorio) . "\n";
